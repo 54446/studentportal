548e02a776b8bf570c5c510f1c80f54e397bda40
Conny Moseri <conny@jvrafrica.co.za>
2025-03-08 21:24:58 +0200

    user interface

--- src/app/pages/register-login/register-login.component.ts
+++ src/app/pages/register-login/register-login.component.ts
@@ -1,11 +1,70 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-register-login',
+//   imports: [],
+//   templateUrl: './register-login.component.html',
+//   styleUrl: './register-login.component.scss'
+// })
+// export class RegisterLoginComponent {
+
+//   const container = document.querySelector('.container');
+//   const registerBtn = document.querySelector('.register-btn');
+//   const loginBtn = document.querySelector('.login-btn');
+
+//   registerBtn.addEventListener('click', () => {
+//     container.classList.add('active');
+//   });
+
+//   loginBtn.addEventListener('click', () => {
+//     container.classList.remove('active');
+//   });
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-register-login',
-  imports: [],
+  standalone: true,
   templateUrl: './register-login.component.html',
   styleUrl: './register-login.component.scss'
 })
-export class RegisterLoginComponent {
+export class RegisterLoginComponent implements AfterViewInit {
 
+  private container!: HTMLElement | null;
+  private registerBtn!: HTMLElement | null;
+  private loginBtn!: HTMLElement | null;
+
+  ngAfterViewInit(): void {
+    this.container = document.querySelector('.container');
+    this.registerBtn = document.querySelector('.register-btn');
+    this.loginBtn = document.querySelector('.login-btn');
+
+    if (this.registerBtn && this.container) {
+      this.registerBtn.addEventListener('click', () => {
+        this.container?.classList.add('active');
+      });
+    }
+
+    if (this.loginBtn && this.container) {
+      this.loginBtn.addEventListener('click', () => {
+        this.container?.classList.remove('active');
+      });
+    }
+  }
 }
+
